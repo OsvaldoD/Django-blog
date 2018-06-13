a4ae8d3fb71d9fd9f158177905857a89abdb4113
Osvaldo Domingos <osvaldod.mariquele@gmail.com>
2018-06-13 00:24:30 -0700

    Commit Test

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,8 @@ from .models import Post, Autor
 # Create your views here.
 def index(request):
     posts = Post.objects.order_by('-data')
-    context = {'posts': posts}
+    body = ''
+    for post in posts:
+        body = post.corpo[:350]
+    context = {'posts': posts,'corpo':body}
     return render(request, 'blog/index.html', context)
